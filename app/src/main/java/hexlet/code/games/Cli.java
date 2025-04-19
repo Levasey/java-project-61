cfba885ef68e5c5aa532ca93c33ea9d23490e7c7
Your Name <you@example.com>
2025-04-19 08:11:33 +0600

    Delete Game update Engine

--- app/src/main/java/hexlet/code/games/Cli.java
+++ app/src/main/java/hexlet/code/games/Cli.java
@@ -8,6 +8,6 @@ public class Cli {
         System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        System.out.println("Hello, " + name);
+        System.out.println("Hello, " + name + "!");
     }
 }
